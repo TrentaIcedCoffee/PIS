2cc932db484c99fa70aa4266d8fb18543762bfec
TrentaIcedCoffee <jin@ucdavis.edu>
2017-10-28 04:22:11 -0700

    FIXED script db pwd

--- private/db_depts_import.js
+++ private/db_depts_import.js
@@ -1,39 +1,24 @@
+// TODO -> findOne
 /*
 This script imports data from MySQL to Mongo, use with care.
 Password remain unset
- */
-var mysqlPwd = "********";
-var mongoPwd = "********";
+*/
+var mongoUser = '********'; // config.dbUser
+var mongoPwd = '********'; // config.dbPwd
+var mysqlUser = '********';
+var mysqlPwd = '********';
+var mysqlDbName = '********';
+var mysqlHost = '********';
 
 var MongoClient = require('mongodb').MongoClient;
 var mysql = require('mysql');
 
 var config = {
-    dbUri: `mongodb://sissdata:${mongoPwd}@siss-shard-00-00-aoiln.mongodb.net:27017,siss-shard-00-01-aoiln.mongodb.net:27017,siss-shard-00-02-aoiln.mongodb.net:27017/test?ssl=true&replicaSet=siss-shard-0&authSource=admin`,
+    dbUri: `mongodb://${mongoUser}:${mongoPwd}@siss-shard-00-00-aoiln.mongodb.net:27017,siss-shard-00-01-aoiln.mongodb.net:27017,siss-shard-00-02-aoiln.mongodb.net:27017/test?ssl=true&replicaSet=siss-shard-0&authSource=admin`,
     dbDepts: 'depts'
 };
+
 var data = [];
-
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-}
-
-var regulateString = function(string) {
-    if (string === null) {
-        return null;
-    } else if (typeof string !== 'string') {
-        throw new TypeError('expect string, got ' + typeof string);
-    }
-
-    return string.trim().replaceAll('&', 'and').replaceAll(' ', '_').toLowerCase();
-}
-
-var dataOf = function(data) {
-    for (var i in data) {
-        data[i] = regulate(data[i]);
-    }
-}
 
 var Model = function(name, college, cluster) {
     this.name = name;
@@ -41,23 +26,36 @@ var Model = function(name, college, cluster) {
     this.cluster = cluster === undefined ? null : cluster;
 }
 
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+}
+
+// replaceAll & -> and, space -> _
+var regulateString = function(string) {
+    if (string === null || typeof string !== 'string') {
+        return string;
+    }
+    return string.trim().replaceAll('&', 'and').replaceAll(' ', '_').toLowerCase();
+}
+
 var regulateData = function(data) {
     for (var i in data) {
-        if (typeof data[i] === 'string') {
+        if (!data[i] && typeof data[i] === 'string') {
             data[i] = regulateString(data[i]);
         }
     }
-
     return data;
 }
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: mysqlHost,
+    user: mysqlUser,
     password: mysqlPwd,
-    database: 'siss'
+    database: mysqlDbName
 });
 
+// TODO -> findOne
 connection.connect(function(err) {
     if (err) {
         throw err;
