2acdf9c13cf0b489d2d10f6e7e5878a434676aee
TrentaIcedCoffee <jin@ucdavis.edu>
2017-10-28 12:35:51 -0700

    FIXED user crud

--- private/config.js
+++ private/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.dbUser = '********';
-config.dbPwd = '********';
+config.dbUser = '*';
+config.dbPwd = '*';
 config.dbUri = `mongodb://${config.dbUser}:${config.dbPwd}@siss-shard-00-00-aoiln.mongodb.net:27017,siss-shard-00-01-aoiln.mongodb.net:27017,siss-shard-00-02-aoiln.mongodb.net:27017/test?ssl=true&replicaSet=siss-shard-0&authSource=admin`;
 config.dbUsers = 'users';
 config.dbDepts = 'depts';
--- server/api/users/index.js
+++ server/api/users/index.js
@@ -1,9 +1,24 @@
+// TODO better crud
 var usersRouter = require('express').Router();
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var logger = require(`${rootUri}/private/logger`);
 var config = require(`${rootUri}/private/config`);
 var util = require(`${rootUri}/private/util`);
+
+var Data = function(req) {
+    this.email = req.body.email;
+    this.password = req.body.password;
+    this.name = req.body.name;
+    this.phone = req.body.phone;
+    this.lifetime = req.body.lifetime;
+    this.depts = req.body.depts;
+    this.visa_types = req.body.visa_types;
+    this.working_email = req.body.working_email;
+    this.supervisor_name = req.body.supervisor_name;
+    this.supervisor_email = req.body.supervisor_email;
+    this.note = req.body.note;
+}
 
 usersRouter.route('/')
     .get(function(req, res) {
@@ -12,12 +27,35 @@ usersRouter.route('/')
             if (err) {
                 throw err;
             }
-            db.collection(config.dbUsers).find().toArray(function(err, result) {
+            db.collection(config.dbUsers).find().toArray(function(err, results) {
                 if (err) {
                     throw err;
                 }
-
-                res.json(result);
+                res.json(results);
+                db.close();
+            });
+        });
+    })
+    .put(function(req, res) {
+        console.log('PUT localhost:3000/users');
+        console.log(req.body);
+        var data = new Data(req);
+        MongoClient.connect(config.dbUri, function(err, db) {
+            if (err) {
+                throw err;
+            }
+            db.collection(config.dbUsers).insertOne(data, function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                data._id = result.insertedId; // data._id: ObjectId
+                db.collection(config.dbUsers).findOne({_id: data._id}, function(err, result) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.json(result);
+                    db.close();
+                });
             });
         });
     });
@@ -25,15 +63,61 @@ usersRouter.route('/')
 usersRouter.route('/:id')
     .get(function(req, res) {
         console.log('GET localhost:3000/users/id');
+        console.log(req.params.id);
         MongoClient.connect(config.dbUri, function(err, db) {
             if (err) {
                 throw err;
             }
-            db.collection(config.dbUsers).find({_id: new ObjectId(req.params.id)}).toArray(function(err, result) {
+            db.collection(config.dbUsers).findOne({_id: new ObjectId(req.params.id)}, function(err, result) {
                 if (err) {
                     throw err;
                 }
                 res.json(result);
+                db.close();
+            });
+        });
+    })
+    .post(function(req, res) {
+        console.log('POST localhost:3000/users/id');
+        console.log(req.params.id);
+        var data = new Data(req);
+        MongoClient.connect(config.dbUri, function(err, db) {
+            if (err) {
+                throw err;
+            }
+            db.collection(config.dbUsers).updateOne({_id: new ObjectId(req.params.id)}, data, function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                db.collection(config.dbUsers).findOne({_id: new ObjectId(req.params.id)}, function(err, result) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.json(result);
+                    db.close();
+                });
+            });
+        });
+    })
+    .delete(function(req, res) {
+        console.log('DELETE localhost:3000/users/id');
+        console.log(req.params.id);
+        MongoClient.connect(config.dbUri, function(err, db) {
+            if (err) {
+                throw err;
+            }
+            db.collection(config.dbUsers).findOne({_id: new ObjectId(req.params.id)}, function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                var data = result;
+                db.collection(config.dbUsers).removeOne({_id: new ObjectId(req.params.id)}, function(err, result) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.json(data);
+                    db.close();
+                });
             });
         });
     });
