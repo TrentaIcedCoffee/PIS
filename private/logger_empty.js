ec86e895205bd330d4145b9a6ba4dcef45f81213
TrentaIcedCoffee <jin@ucdavis.edu>
2017-10-28 12:47:00 -0700

    FIXED script logger_empty description

--- private/logger_empty.js
+++ private/logger_empty.js
@@ -1,3 +1,6 @@
+/*
+    This script is used to empty log, use with care
+*/
 var path = require('path');
 global.rootUri = path.join(__dirname, '..');
 var logger = require(`${rootUri}/private/logger`);
