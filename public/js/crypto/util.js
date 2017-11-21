4a8e2c9363cdd295a87168fb7f38b7ea22188ac7
TrentaIcedCoffee <jin@ucdavis.edu>
2017-11-20 18:42:19 -0800

    IMPL crypto util

--- public/js/crypto/util.js
+++ public/js/crypto/util.js
@@ -2,7 +2,7 @@
 var getBase64Encoded = function(rawStr) {
     var wordArray = CryptoJS.enc.Utf8.parse(rawStr);
     var result = CryptoJS.enc.Base64.stringify(wordArray);
-    reture result;
+    return result;
 };
 
 var getBase64Decoded = function(encStr) {
@@ -10,3 +10,12 @@ var getBase64Decoded = function(encStr) {
     var result = wordArray.toString(CryptoJS.enc.Utf8);
     return result;
 };
+
+var getJwt = function(header, payload, secret) {
+    var headerBase64 = getBase64Encoded(header);
+    var payloadBase64 = getBase64Encoded(payload);
+    var signature = CryptoJS.HmacSHA256(headerBase64 + '.' + payloadBase64, secret);
+    var signatureBase64 = CryptoJS.enc.Base64.stringify(signature);
+    var jwt = headerBase64 + '.' + payloadBase64 + '.' + signatureBase64;
+    return jwt;
+}
