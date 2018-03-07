875af34635ff2c9cae6c4b631268dac59490b59f
TrentaIcedCoffee <jin@ucdavis.edu>
2018-03-06 22:40:03 -0800

    FIXED correct uri

--- private/passport.js
+++ private/passport.js
@@ -15,11 +15,15 @@ for (var field of passport_fields) {
 }
 
 passport.db_dev_uri = `mongodb://${passport.db_dev_username}:${passport.db_dev_password}@` +
-    `${passport.db_dev_dbname}-00-00-${passport.db_dev_zone}.mongodb.net:27017` +
-    `/test?ssl=true&replicaSet=siss-shard-0&authSource=admin`;
+    `${passport.db_dev_dbname}-00-00-${passport.db_dev_zone}.mongodb.net:27017,` +
+    `${passport.db_dev_dbname}-00-01-${passport.db_dev_zone}.mongodb.net:27017,` +
+    `${passport.db_dev_dbname}-00-02-${passport.db_dev_zone}.mongodb.net:27017` +
+    `/test?ssl=true&replicaSet=${passport.db_dev_dbname}-0&authSource=admin`;
 
 passport.db_production_uri = `mongodb://${passport.db_production_username}:${passport.db_production_password}@` +
-    `${passport.db_production_dbname}-00-00-${passport.db_production_zone}.mongodb.net:27017` +
-    `/test?ssl=true&replicaSet=siss-shard-0&authSource=admin`;
+    `${passport.db_production_dbname}-00-00-${passport.db_production_zone}.mongodb.net:27017,` +
+    `${passport.db_production_dbname}-00-01-${passport.db_production_zone}.mongodb.net:27017,` +
+    `${passport.db_production_dbname}-00-02-${passport.db_production_zone}.mongodb.net:27017` +
+    `/test?ssl=true&replicaSet=${passport.db_production_dbname}-0&authSource=admin`;
 
 module.exports = passport;
